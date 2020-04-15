f7edb39921e2bfbdbb8f86b8cde282af8d8295d1
Michael Moura <mikemorcerf@gmail.com>
2020-04-15 17:40:36 -0400

    finish challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((acc, curr)=>{
+      if(curr.type==='income'){
+        acc.income += curr.value;
+      } else {
+        acc.outcome += curr.value;
+      }
+      return acc;
+    }, {income: 0, outcome: 0, total: 0});
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const balance = this.getBalance();
+    if(type==='outcome' && balance.total-value<0){
+      throw Error('Current balance not enough to process transaction.');
+    }
+
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!title || !value || !type) {
+      throw Error(
+        'Missing parameter. Transaction must contain title, value and type',
+      );
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type of transaction must be either income or outcome only.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
